41e265bd89cd0edd97a0ce2dd918711ea3c87c09
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-04-05 10:21:00 +0200

    Added API endpoints. Kader/Adam

---
+++ types/challengeTemp.tsx
@@ -0,0 +1,11 @@
+import { ObjectId } from "mongodb";
+
+export type challenge = {
+  _id: ObjectId;
+  publisher: string;
+  title: string;
+  description: string;
+  category: string;
+  location: string;
+  time: string;
+};
