575dca18a48d6d54df3eeb721c15638ffe166dc6
MysteriousAfg <113422710+MysteriousAfg@users.noreply.github.com>
2023-04-14 12:21:26 +0200

    fixed location icon

--- pages/startsida.tsx
+++ pages/startsida.tsx
@@ -197,13 +197,13 @@ function Challenges() {
             >
               Location
             </label>
-            <div className="flex flex-row">
-              <Image
-                src={"/Location-Icon-Filled.png"}
-                alt={"#"}
-                width={"18"}
-                height={"18"}
-              ></Image>
+             
+            <div className="flex flex-row items-center">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+              <path fill="#000" fill-rule="evenodd" d="m9.94 12.646-2.248-.749c-2.353-.784-3.53-1.176-3.53-1.897 0-.72 1.177-1.113 3.53-1.897l8.513-2.838c1.656-.552 2.484-.828 2.921-.391.437.437.161 1.265-.39 2.92l-2.839 8.514c-.784 2.353-1.176 3.53-1.897 3.53-.72 0-1.113-1.177-1.897-3.53l-.75-2.247 4.354-4.354a1 1 0 0 0-1.414-1.414l-4.354 4.353Z" 
+              clip-rule="evenodd"/>
+            </svg>
               <input
                 className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-4"
                 id="location"
