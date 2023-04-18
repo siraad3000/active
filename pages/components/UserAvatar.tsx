b3654463c49250f012dd699f2a3e8b922e42af68
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-04-18 12:16:33 +0200

    cleaned code

--- pages/components/UserAvatar.tsx
+++ pages/components/UserAvatar.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 export default function Avatar() {
   return (
-    <div>
-      <div className="flex items-end justify-end mr-2">
-        <Image src="/user-avatar.png" alt="Avatar" width={35} height={35} />
-      </div>
-    </div>
+    <Image
+      src="/user-avatar.png"
+      alt="Avatar"
+      width={35}
+      height={35}
+      className="flex items-end justify-end mr-5"
+    />
   );
 }
