bff427aff80d2a71e5c7989b9dbf632715667b4c
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-04-22 15:37:46 +0200

    vercel config

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,39 +7,38 @@ import { useEffect, useState } from "react";
 interface Props {}
 
 const Profile = () => {
-  const [users, setUsers] = useState<Users[]>([]);
-  useEffect(() => {
-    async function fetchUsers() {
-      const res = await fetch("/api/users");
-      const data = await res.json();
-      setUsers(data);
-    }
-    fetchUsers();
-  }, []);
-  return (
-    <div>
-      <Header />
-      <div className="bg-active-white mt-24 ">
-        <div className="bg-active-purple h-40">
-          <img src="Kim-Banner.jpg" alt="" className="w-full h-full" />
-        </div>
-        <div className="mx-3">
-          <div>
-            <div className="z-10 h-10 w-10">
-              <Avatar />
-            </div>
-            <p className="text-lg leading-6 font-bold">{users[2].name}</p>
-            <p>{users[2].location}</p>
-
-            <div className="h-10 "> Här kommer bilder sen</div>
-            <div>
-              <p>{users[2].description}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // const [users, setUsers] = useState<Users[]>([]);
+  // useEffect(() => {
+  //   async function fetchUsers() {
+  //     const res = await fetch("/api/users");
+  //     const data = await res.json();
+  //     setUsers(data);
+  //   }
+  //   fetchUsers();
+  // }, []);
+  // return (
+  //   <div>
+  //     <Header />
+  //     <div className="bg-active-white mt-24 ">
+  //       <div className="bg-active-purple h-40">
+  //         <img src="Kim-Banner.jpg" alt="" className="w-full h-full" />
+  //       </div>
+  //       <div className="mx-3">
+  //         <div>
+  //           <div className="z-10 h-10 w-10">
+  //             <Avatar />
+  //           </div>
+  //           <p className="text-lg leading-6 font-bold">{users[2].name}</p>
+  //           <p>{users[2].location}</p>
+  //           <div className="h-10 "> Här kommer bilder sen</div>
+  //           <div>
+  //             <p>{users[2].description}</p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Profile;
