6b6c04d9763ebc2d2ad3883e62ea7f0e10e90653
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-04-22 14:31:39 +0200

    Template config

--- types/challengeTemp.tsx
+++ types/challengeTemp.tsx
@@ -3,7 +3,7 @@ import { ObjectId } from "mongodb";
 export type Challenge = {
   _id?: ObjectId;
   title: string;
-  publisher: string;
+  publisher?: string;
   description: string;
   level: string;
   showFor: string;
