28b1ca9e079bd50b1cad341704b692b459de326b
Grimlli <adam.hedberg.ahj@gmail.com>
2023-04-26 14:46:02 +0200

    Changes to get to be able to get one user50% /Adam

--- pages/api/challenges.tsx
+++ pages/api/challenges.tsx
@@ -1,42 +1,62 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { ObjectId } from "mongodb";
-import clientPromise from "../../lib/mongodb";
-import { Challenge } from "../../types/challengeTemp";
+import type { NextApiRequest, NextApiResponse } from "next"
+import { ObjectId } from "mongodb"
+import clientPromise from "../../lib/mongodb"
+import { Challenge } from "../../types/challengeTemp"
 
 export const getChallenges = async (): Promise<Challenge[]> => {
-  const mongoClient = await clientPromise;
+  const mongoClient = await clientPromise
   const challenges = (await mongoClient
     .db("active")
     .collection("challenges")
     .find()
-    .toArray()) as Challenge[];
-  return challenges;
-};
+    .toArray()) as Challenge[]
+  return challenges
+}
+
+export const getOneUsersChallenges = async (
+  x: string
+): Promise<Challenge[]> => {
+  const mongoClient = await clientPromise
+  const challenges = (await mongoClient
+    .db("active")
+    .collection("challenges")
+    .find({ title: x })
+    .toArray()) as Challenge[]
+  return challenges
+}
 
 export const addChallenge = async (challenge: Challenge): Promise<ObjectId> => {
-  const mongoClient = await clientPromise;
+  const mongoClient = await clientPromise
 
   const response = await mongoClient
     .db("active")
     .collection("challenges")
-    .insertOne(challenge);
+    .insertOne(challenge)
 
-  return response.insertedId;
-};
+  return response.insertedId
+}
 
 export const challengesHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   if (req.method === "POST") {
-    const newChallenge = req.body as Challenge;
-    const createdChallenge = await addChallenge(newChallenge);
-    res.status(201).json(createdChallenge);
+    const newChallenge = req.body as Challenge
+    const createdChallenge = await addChallenge(newChallenge)
+    res.status(201).json(createdChallenge)
   } else if (req.method === "GET") {
-    const challenges = await getChallenges();
-    res.status(200).json(challenges);
+    const { username } = req.query
+    console.log("in i get ")
+    if (username) {
+      console.log("in i username")
+      const challenges = await getOneUsersChallenges(username as string)
+      res.status(200).json(challenges)
+    } else {
+      const challenges = await getChallenges()
+      res.status(200).json(challenges)
+    }
   } else {
-    res.status(405).end();
+    res.status(405).end()
   }
-};
-export default challengesHandler;
+}
+export default challengesHandler
