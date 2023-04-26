f8aa64181b5a78b45cbaca04df8f0bbe3955d8a4
Grimlli <adam.hedberg.ahj@gmail.com>
2023-04-26 13:24:47 +0200

    Now Profile template works/Adam

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,44 +1,44 @@
-import { NextPage } from "next";
-import Header from "../components/Header";
-import { Users } from "../types/usersTemp";
-import FooterNavbar from "../components/FooterNavbar";
-import Avatar from "../components/UserAvatar";
-import { useEffect, useState } from "react";
+import { NextPage } from "next"
+import Header from "../components/Header"
+import { Users } from "../types/usersTemp"
+import FooterNavbar from "../components/FooterNavbar"
+import Avatar from "../components/UserAvatar"
+import { useEffect, useState } from "react"
 interface Props {}
 
 const Profile = () => {
-  // const [users, setUsers] = useState<Users[]>([]);
-  // useEffect(() => {
-  //   async function fetchUsers() {
-  //     const res = await fetch("/api/users");
-  //     const data = await res.json();
-  //     setUsers(data);
-  //   }
-  //   fetchUsers();
-  // }, []);
-  // return (
-  //   <div>
-  //     <Header />
-  //     <div className="bg-active-white mt-24 ">
-  //       <div className="bg-active-purple h-40">
-  //         <img src="Kim-Banner.jpg" alt="" className="w-full h-full" />
-  //       </div>
-  //       <div className="mx-3">
-  //         <div>
-  //           <div className="z-10 h-10 w-10">
-  //             <Avatar />
-  //           </div>
-  //           <p className="text-lg leading-6 font-bold">{users[2].name}</p>
-  //           <p>{users[2].location}</p>
-  //           <div className="h-10 "> Här kommer bilder sen</div>
-  //           <div>
-  //             <p>{users[2].description}</p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-};
+  const [users, setUsers] = useState<Users[]>([])
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch("/api/users")
+      const data = await res.json()
+      setUsers(data)
+    }
+    fetchUsers()
+  }, [])
+  return (
+    <div>
+      <Header />
+      <div className="bg-active-white mt-24 ">
+        <div className="bg-active-purple h-40">
+          <img src="Kim-Banner.jpg" alt="" className="w-full h-full" />
+        </div>
+        <div className="mx-3">
+          <div>
+            <div className="z-10 h-10 w-10">
+              <Avatar />
+            </div>
+            <p className="text-lg leading-6 font-bold">{users[2]?.name}</p>
+            <p>{users[2]?.location}</p>
+            <div className="h-10 "> Här kommer bilder sen</div>
+            <div>
+              <p>{users[2]?.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Profile;
+export default Profile
