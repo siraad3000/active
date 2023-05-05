b2b76ec96b86b2bd4c8739e654607ec2fd1c2e65
whatever <you@example.com>
2023-05-05 11:31:56 +0200

    added sub menu to friends header

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,27 +1,25 @@
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import Head from "next/head";
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import Head from "next/head"
 
-import { useEffect, useState } from "react";
-import FooterNavbar from "../components/FooterNavbar";
-import Header from "../components/Header";
-import Avatar from "@/components/UserAvatar";
+import { useEffect, useState } from "react"
+import FooterNavbar from "../components/FooterNavbar"
+import Header from "../components/Header"
+import Avatar from "@/components/UserAvatar"
 
 const Friends = () => {
-  
-  
   return (
     <div className=" bg-active-white min-h-screen friends">
       <div className="-mt-6">
         <Header />
-        <div className=" text-lg">
-          
-        </div>
+        <div className=" text-lg"></div>
+        <div className="v">Vänner</div>
+        <div className="g">Grupper</div>
       </div>
 
       <FooterNavbar />
     </div>
-  );
-};
+  )
+}
 
-export default Friends;
+export default Friends
