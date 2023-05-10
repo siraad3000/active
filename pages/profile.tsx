3cb07215383ce96cd2976f457ec00f8278717401
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-05-10 09:42:05 +0200

    profile changes

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,21 +1,17 @@
-import Image from "next/image"
-import Avatar from "../components/UserAvatar"
-import Profileheader from "@/components/Headerprofile"
-import { GetSessionParams, getSession, useSession } from "next-auth/react"
-import clientPromise from "@/lib/mongodb"
-import { Challenge } from "@/types/challengeTemp"
-import { User } from "next-auth"
-import { useEffect, useState } from "react"
-import challenges from "./api/challenges"
-import DisplayChallenges from "@/components/DisplayChallenges"
-import { Session } from "inspector"
+import Image from "next/image";
+import Avatar from "../components/UserAvatar";
+import Profileheader from "@/components/Headerprofile";
+import { GetSessionParams, getSession, useSession } from "next-auth/react";
+import clientPromise from "@/lib/mongodb";
+import { Challenge } from "@/types/challengeTemp";
+import DisplayChallenges from "@/components/DisplayChallenges";
 
 interface Props {
-  challenges: Challenge[]
+  challenges: Challenge[];
 }
 
 const Profile = ({ challenges }: Props) => {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   return (
     <div className="flex-wrap bg-active-offWHite h-screen">
@@ -32,7 +28,7 @@ const Profile = ({ challenges }: Props) => {
                 sizes="100vw"
                 style={{ width: "100%" }}
               />
-              
+
               <div className="px-4 relative bottom-11">
                 <Avatar
                   width={80}
@@ -41,49 +37,40 @@ const Profile = ({ challenges }: Props) => {
                   className="rounded-full border border-active-black"
                 />
                 <div className=" absolute right-2 top-12">
-               
-                    <Image
-                      src="/penna.svg"
-                      alt="Avatar"
-                      width={25}
-                      height={10}
-                    />
-                  
-
+                  <Image src="/penna.svg" alt="Avatar" width={25} height={10} />
                 </div>
                 <p className="font-[Inter] text-xl text-active-purple pt-2 flex justify-between ">
                   {session?.user?.name}
-                  
-                  
                 </p>
                 <div className="flex justify-between">
-                <p>999 år, Stockholm </p>
-                
+                  <p>999 år, Stockholm </p>
                 </div>
                 <div className="pt-2 font-[Inter] text-base text-active-purple  ">
-             <p>Mina Träningsintressen</p>
-              <div className=" mt-2 flex space-x-6"> 
-              <Image
+                  <p>Mina Träningsintressen</p>
+                  <div className=" mt-2 flex space-x-6">
+                    <Image
                       src="/pingis.svg"
                       alt="Avatar"
                       width={80}
                       height={80}
                     />
-                      <Image
+                    <Image
                       src="/simning.svg"
                       alt="Avatar"
                       width={80}
                       height={60}
                     />
-                    
-            
-              </div>
-           </div>
-            
-            <p className="pt-2">Jag gillar spela pingis och jag gillar äta kebab och jag gillar inte simma och jag gillar tennis sen gillar inte att handla men jag tycker det roligt att spela men annars så vet jag inte vad jag skriver hallooooooooooooo</p>
+                  </div>
+                </div>
+
+                <p className="pt-2">
+                  Jag gillar spela pingis och jag gillar äta kebab och jag
+                  gillar inte simma och jag gillar tennis sen gillar inte att
+                  handla men jag tycker det roligt att spela men annars så vet
+                  jag inte vad jag skriver hallooooooooooooo
+                </p>
               </div>
             </div>
-            
           </div>
           <div className="bg-active-offWHite px-4 lg:shadow-lg">
             <div className="flex items-center justify-between py-3">
@@ -92,16 +79,16 @@ const Profile = ({ challenges }: Props) => {
               </p>
               <p>Redigera</p>
             </div>
-            <DisplayChallenges challenges={challenges} className="" />
+            <DisplayChallenges challenges={challenges} className="pb-10" />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export async function getServerSideProps(context: GetSessionParams) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
@@ -109,21 +96,21 @@ export async function getServerSideProps(context: GetSessionParams) {
         destination: "/",
         permanent: false,
       },
-    }
+    };
   }
-  const mongoClient = await clientPromise
+  const mongoClient = await clientPromise;
   const challenges = await mongoClient
     .db("active")
     .collection("challenges")
     .find({ idPublisher: session?.user?.id })
-    .toArray()
+    .toArray();
 
   const serializedChallenges = challenges.map((challenge) => {
-    const { _id, ...rest } = challenge
-    return { ...rest, _id: _id.toString() }
-  })
-  console.log("guego" + session)
-  return { props: { challenges: serializedChallenges } }
+    const { _id, ...rest } = challenge;
+    return { ...rest, _id: _id.toString() };
+  });
+  console.log("guego" + session);
+  return { props: { challenges: serializedChallenges } };
 }
 
-export default Profile
+export default Profile;
