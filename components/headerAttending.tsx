ff6910bb5f4fd8ee17774ee6f5a9376e75b00900
Grimlli <adam.hedberg.ahj@gmail.com>
2023-05-12 10:12:09 +0200

    Created boked page, change header/MOB-ALL

---
+++ components/headerAttending.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image"
+import Avatar from "./UserAvatar"
+import { useSession } from "next-auth/react"
+import router from "next/router"
+import BackButton from "./BackButton"
+import HideForm from "./HideForm"
+export default function HeaderAktiviter() {
+  const { data: session } = useSession()
+
+  return (
+    <div>
+      <header className="fixed top-0 flex  items-end w-full h-24 border-t border-gray-200 bg-active-white z-50">
+        <div className="flex justify-center mb-5 w-full items-end">
+          <div className="w-1/4 h-1/2">
+            <div className=" pl-4 flex space-x-3 justify-start items-end">
+              <Image src="/notice.svg" alt="Avatar" width={25} height={25} />
+
+              <Image src="/search.svg" alt="Avatar" width={25} height={25} />
+            </div>
+          </div>
+
+          <div className="w-1/2 h-1/2 flex justify-center">
+            <Image
+              src={"/active-logo-medium.png"}
+              alt={"#"}
+              width={"120"}
+              height={"120"}
+            />
+          </div>
+          <div
+            className=" w-1/4 h-1/2 pr-4 flex justify-end cursor-pointer"
+            onClick={() => router.push("/profile")}
+          >
+            <Avatar
+              src={session?.user?.image}
+              alt="avatar"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      </header>
+      <div className="fixed w-full lg:flex justify-center shadow-md z-40 bg-active-white">
+        <div className="pt-28 flex justify-center lg:w-1/2 shadow-lg">
+          <div className="w-1/2 flex justify-center border-b-2 border-active-purple font-bold  ">
+            Bokade
+          </div>
+          <div className=" w-1/2 flex justify-center  ">Sparade</div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/aktiviteter.tsx
+++ pages/aktiviteter.tsx
@@ -1,28 +1,56 @@
-import FooterNavbar from "@/components/FooterNavbar";
-import Header from "@/components/Header";
+import DisplayChallenges from "@/components/DisplayChallenges"
+import FooterNavbar from "@/components/FooterNavbar"
+import Header from "@/components/Header"
+import clientPromise from "@/lib/mongodb"
+import { GetSessionParams, getSession, useSession } from "next-auth/react"
+import challenges from "./api/challenges"
+import { Challenge } from "@/types/challengeTemp"
+import HeaderAktiviter from "@/components/headerAttending"
 
-const Aktiviteter = () => {
+interface Props {
+  challenges: Challenge[]
+}
+
+const Aktiviteter = ({ challenges }: Props) => {
+  const { data: session } = useSession()
   return (
-    <div className="h-screen w-screen bg-active-white ">
+    <div className="h-screen w-screen bg-active-offWHite ">
       <main>
-        <Header />
-        <div className="lg:flex justify-center ">
-          <div className="pt-28 flex justify-center lg:w-1/2 shadow-lg">
-            <div className="w-1/2 flex justify-center  border-b-2 border-active-purple font-bold  ">
-              Bokade
-            </div>
-            <div className=" w-1/2 flex justify-center  ">Sparade</div>
-          </div>
-        </div>
+        <HeaderAktiviter />
+
         <div className="lg:flex justify-center p-5">
-          <div className=" bg-active-purple lg:w-1/2">
-            <div>Här kommer vänner</div>
-          </div>
+          <DisplayChallenges challenges={challenges} className="pb-6" />
         </div>
         <FooterNavbar />
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default Aktiviteter;
+export async function getServerSideProps(context: GetSessionParams) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+  const mongoClient = await clientPromise
+  const challenges = await mongoClient
+    .db("active")
+    .collection("challenges")
+    .find({ attending: session?.user?.id })
+    .toArray()
+
+  const serializedChallenges = challenges.map((challenge) => {
+    const { _id, ...rest } = challenge
+    return { ...rest, _id: _id.toString() }
+  })
+  console.log("guego" + session)
+  return { props: { challenges: serializedChallenges } }
+}
+
+export default Aktiviteter
