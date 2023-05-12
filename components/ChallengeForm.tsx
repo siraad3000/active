7398fc0270bd62a415d9fb2131990a1bcb0b687a
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-05-12 15:15:49 +0200

    Challenge form edit

--- components/ChallengeForm.tsx
+++ components/ChallengeForm.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
-import { Challenge } from "@/types/challengeTemp"
-import { useSession } from "next-auth/react"
-import HideForm from "./HideForm"
-import { useFormik } from "formik"
+import Image from "next/image";
+import { Challenge } from "@/types/challengeTemp";
+import { useSession } from "next-auth/react";
+import HideForm from "./HideForm";
+import { useFormik } from "formik";
 
 type ChallengeFormProps = {
-  onSubmitChallenge: (challenge: Challenge) => void
-}
+  onSubmitChallenge: (challenge: Challenge) => void;
+};
 export default function ChallengeForm({
   onSubmitChallenge,
 }: ChallengeFormProps) {
-  const { data: session, status } = useSession()
-  const currentYear = new Date().getFullYear()
+  const { data: session, status } = useSession();
+  const currentYear = new Date().getFullYear();
 
   const formik = useFormik({
     initialValues: {
@@ -20,9 +20,11 @@ export default function ChallengeForm({
       location: "",
       date: "",
       time: "",
+      level: "",
+      showFor: "",
     },
     onSubmit,
-  })
+  });
   async function onSubmit(values: any) {
     const newChallenge: Challenge = {
       publisher: session?.user?.name,
@@ -31,12 +33,14 @@ export default function ChallengeForm({
       location: values.location,
       time: values.sTime + "-" + values.fTime,
       date: values.date,
+      level: values.level,
+      showFor: values.showFor,
       pictureId: session?.user?.image,
       publisherId: session?.user.id,
-      attending: [session?.user.id],
+      attending: [],
       marked: [],
-    }
-    onSubmitChallenge(newChallenge)
+    };
+    onSubmitChallenge(newChallenge);
   }
   return (
     <div className="bg-active-white">
@@ -130,6 +134,73 @@ export default function ChallengeForm({
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="level"
+            >
+              Välj nivå
+            </label>
+            <div className="flex flex-row items-center justify-start ">
+              <div className="flex items-center w-32 h-7">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 2 dark:bg-gray-700 dark:border-gray-600 hidden peer/lätt"
+                  type="radio"
+                  name="level"
+                  id="level"
+                  value={"Lätt"}
+                  checked={formik.values.level === "Lätt"}
+                  onChange={formik.handleChange}
+                />
+                <label
+                  htmlFor="level"
+                  className="w-full h-full py-3 text-sm font-medium text-center peer-checked/lätt:bg-active-purple
+                    peer-checked/lätt:text-active-white border border-gray-200 rounded dark:border-gray-700 flex items-center justify-center "
+                >
+                  Lätt
+                </label>
+              </div>
+
+              <div className="flex items-center pl-4  w-32 h-7">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 2 dark:bg-gray-700 dark:border-gray-600 hidden peer/medel"
+                  type="radio"
+                  name="level"
+                  id="level2"
+                  value={"Medel"}
+                  checked={formik.values.level === "Medel"}
+                  onChange={formik.handleChange}
+                />
+                <label
+                  htmlFor="level2"
+                  className="w-full h-full py-3 text-sm font-medium text-center peer-checked/medel:bg-active-purple 
+           peer-checked/medel:text-active-white border border-gray-200 rounded dark:border-gray-700 flex items-center justify-center"
+                >
+                  medel
+                </label>
+              </div>
+
+              <div className="flex items-center pl-4  w-32 h-7">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 2 dark:bg-gray-700 dark:border-gray-600 hidden peer/test"
+                  type="radio"
+                  name="level"
+                  id="level3"
+                  value={"Hög"}
+                  checked={formik.values.level === "Hög"}
+                  onChange={formik.handleChange}
+                />
+
+                <label
+                  htmlFor="level3"
+                  className="w-full h-full py-3 text-sm font-medium text-center  peer-checked/test:bg-active-purple
+                 peer-checked/test:text-active-white border border-gray-200 rounded dark:border-gray-700 flex items-center justify-center"
+                >
+                  intensivt
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="description"
             >
               Lägg till beskrivning
@@ -144,12 +215,76 @@ export default function ChallengeForm({
               {...formik.getFieldProps("description")}
             ></textarea>
           </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="showFor"
+            >
+              Visas för
+            </label>
+            <div className="flex flex-row items-center justify-start">
+              <div className="flex items-center w-32 h-7">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:bg-gray-700 dark:border-gray-600 peer/friends hidden"
+                  type="radio"
+                  name="showFor"
+                  id="friends"
+                  value={"Vänner"}
+                  checked={formik.values.showFor === "Vänner"}
+                  onChange={formik.handleChange}
+                />
+                <label
+                  htmlFor="friends"
+                  className="w-full h-full py-3 text-sm font-medium peer-cheaked:text-red-500 text-center peer-checked/friends:bg-active-purple peer-checked/friends:text-active-white border border-gray-200 rounded dark:border-gray-700 flex items-center justify-center"
+                >
+                  Vänner
+                </label>
+              </div>
+
+              <div className="flex items-center pl-4 w-32 h-7">
+                <input
+                  className="text-blue-600 bg-gray-100 border-gray-300 2 dark:bg-gray-700 dark:border-gray-600 hidden peer/grupper"
+                  type="radio"
+                  name="showFor"
+                  id="grupper"
+                  value={"Grupper"}
+                  checked={formik.values.showFor === "Grupper"}
+                  onChange={formik.handleChange}
+                />
+                <label
+                  htmlFor="grupper"
+                  className="w-full h-full py-3 ml-2 text-sm font-medium peer-cheaked:text-red-500 text-center peer-checked/grupper:bg-active-purple peer-checked/grupper:text-active-white border border-gray-200 rounded dark:border-gray-700 flex items-center justify-center"
+                >
+                  Grupper
+                </label>
+              </div>
+              <div className="flex items-center pl-4  w-32 h-7 ">
+                <input
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 2 dark:bg-gray-700 dark:border-gray-600 hidden peer/all"
+                  type="radio"
+                  name="showFor"
+                  id="all"
+                  value={"Offentligt"}
+                  checked={formik.values.showFor === "Offentligt"}
+                  onChange={formik.handleChange}
+                />
+                <label
+                  htmlFor="all"
+                  className="w-full h-full py-3 text-sm font-medium  text-center peer-checked/all:bg-active-purple
+                   peer-checked/all:text-active-white border border-gray-200 
+                   rounded dark:border-gray-700 flex items-center justify-center"
+                >
+                  Offentligt
+                </label>
+              </div>
+            </div>
+          </div>
           <button
             className=" w-full purple1 hover:bg-purple-700 text-active-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
             type="submit"
             id="button"
             onClick={() => {
-              HideForm()
+              HideForm();
             }}
           >
             Publicera
@@ -192,5 +327,5 @@ export default function ChallengeForm({
         </button>
       </div>
     </div>
-  )
+  );
 }
--- types/challengeTemp.tsx
+++ types/challengeTemp.tsx
@@ -1,15 +1,17 @@
-import { ObjectId } from "mongodb"
+import { ObjectId } from "mongodb";
 
 export type Challenge = {
-  _id?: ObjectId
-  title: string
-  publisher?: string | null
-  publisherId: string
-  description: string
-  location: string
-  date: string
-  time: string
-  attending?: string[]
-  marked?: string[]
-  pictureId?: string | null
-}
+  _id?: ObjectId;
+  title: string;
+  publisher?: string | null;
+  publisherId: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  level: string;
+  showFor: string;
+  attending?: string[];
+  marked?: string[];
+  pictureId?: string | null;
+};
